678111fd36a9b7308e20ed9524586929c8bfc124
Atlia-ND14 <atliadaniel14@gmail.com>
2023-11-28 10:25:26 -0800

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'),ord('z') + 1):
-    if chr(i) in ('q','e'):
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) in ('q', 'e'):
         continue
     print('{}'.format(chr(i)), end='')
-
