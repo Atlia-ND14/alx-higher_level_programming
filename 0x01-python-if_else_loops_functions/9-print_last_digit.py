4a5c6a40285ca78fe10957b183e134ba6b23d0d1
Atlia-ND14 <atliadaniel14@gmail.com>
2023-11-30 06:18:57 -0800

    modified

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit)
-    return last_digit
+    if number < 0:
+        number *= -1
+        print("{}".format(number % 10), end="")
+    return(number % 10)
